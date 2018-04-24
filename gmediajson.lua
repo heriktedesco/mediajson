faac1b748f8b2dd9808f4e13d14281d1075f4d31
Gustavo H M Silva <gustavohmsilva@tuta.io>
2018-04-24 14:19:38 -0300

    mediaJSON: Version with a GUI based on Zenity

---
+++ gmediajson.lua
@@ -0,0 +1,8 @@
+#!/usr/bin/env lua5.3
+--[[ 
+     Version of the mediaJSON script with a Graphical Interface:
+     Uses Lenity to provide dialogs for the whole process.
+]]--
+
+-- https://github.com/LPLBrasil/Lenity : Created by Gustavo H M Silva
+l = require 'lenity'
