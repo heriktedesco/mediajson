a184da77032b64c6030f8bdddd90d7e2ebe9d64d
heriktedesco <herik.tedesco@al.infnet.edu.br>
2018-04-27 16:12:27 -0300

    Update gmediajson.lua

--- gmediajson.lua
+++ gmediajson.lua
@@ -1,35 +1,28 @@
 #!/usr/bin/env lua5.3
+-- https://github.com/LPLBrasil/Lenity : Created by Gustavo H M Silva
+l = require 'lenity'
 --[[
      Version of the mediaJSON script with a Graphical Interface:
      Uses Lenity to provide dialogs for the whole process.
 ]]--
 
--- https://github.com/LPLBrasil/Lenity : Created by Gustavo H M Silva
-l = require 'lenity'
-
 nameFile = l.fileselection("JSON")
-
 nameLto = l.entry("Nome da LTO", "Escreva qual LTO deseja gravar.")
 
-commandJson = 'mediajson "'..nameFile..'" "'..nameLto..'"'
-
--- Missing verification "." (point) and call variables.
 for i = nameFile:len(), 1, -1 do
-  if nameFile:sub(i , i) == "/" then
-    filePath, filename = nameFile:sub(1, i), nameFile:sub(i + 1, nameFile:len())
+  if nameFile:sub(i, i) == '.' then _ = i end
+  if nameFile:sub(i, i) == "/" then
+    filePath, filename, filenameNoExtension = nameFile:sub(1, i), nameFile:sub(i+1, nameFile:len()), nameFile:sub(i+1,_)
     break
   end
 end
 
+commandJson = 'cd '..filePath..' && ./mediajson.lua "'..filename..'" "'..nameLto..'"'
+print(commandJson)
 os.execute(commandJson)
 
-jsonFile = string.sub(nameFile, 1, -5)..".json"
-
 if not(nameLto == "") then
-  l.textinfo("Confirmação", jsonFile, nil, nil, nil, nil, nil, 1000, 800)
+  l.textinfo("Confirmação", filenameNoExtension.."json",0,0,0,0,0,1000,800)
 else
   l.info("Confirmação", "Você não escolheu a LTO para ser gravada.")
 end
-
-print(nameFile)
-print(nameLto)
